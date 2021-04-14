3ddaf7bc6010ba3f179abdecac893f8ea9094205
NathanRodriguez04 <naterod26@gmail.com>
2021-04-14 13:08:55 -0500

    apr 14

---
+++ js/external.js
@@ -0,0 +1,12 @@
+alert("Welcome to my Website")
+
+var userinput = prompt("What is your favorite color");
+
+// alert("your favorite color is: " + userinput);
+
+if(userinput === "red"){
+    alert("lets rock")
+}
+else{
+    alert("you chose: " + userinput);
+}
