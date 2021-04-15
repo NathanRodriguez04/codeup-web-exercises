3179a05e6ca1c529135d0e10a583497b726cb45f
NathanRodriguez04 <naterod26@gmail.com>
2021-04-15 17:04:47 -0500

    apr 15

---
+++ js/shorthand-unart-and-jsmethods.js
@@ -0,0 +1,94 @@
+"use strict";
+
+//shorthand assignment
+
+// += /-=
+
+//var name = "nathan";
+
+//name = name + " " +"edwards";
+//console.log(name);
+
+//name += " edwards";
+//console.log(name);
+
+//console.log(name -= "edwards");
+
+//var daysremainingincohort = 114;
+
+//daysremainingincohort -= 4;
+
+//console.log(daysremainingincohort);
+
+
+// TODO: Refactor the mini exercise we did earlier on arithmetic operators to use variables and their related shorthand assignment operators, then log out the result
+//  ie:
+//     var result = 5;
+//     console.log(result += 1);
+
+
+//var add = 3;
+//add += 1;
+//console.log(add);
+
+//var sub =5
+//sub -= 1;
+//console.log(sub);
+
+
+//var multiply =2
+//multiply *=2;
+//console.log(multiply);
+
+//var divide =8
+//divide /=2;
+//console.log(divide);
+
+
+//var modulo =4
+//modulo %=8
+//console.log(modulo);
+
+//var makeitanumber = "doge";
+
+//console.log(-makeitanumber);
+
+// increment/decrement
+
+//var num = 10;
+//console.log(num++);   //num += 1
+//console.log(num);
+
+// js built in methods
+
+// tosting();
+
+//var num = 5 * 5 ;
+//num += num.toString();
+
+//console.log(typeof num.toString());
+//console.log(typeof num);
+
+//var pi = 3.1415926535
+
+//console.log(pi.toFixed(0));
+
+//number()
+
+//console.log(Number("nathan"));
+
+// isnan() - checks to see if the value provides is not-a-number
+
+
+//console.log(!isNaN("nathan"));   // add a bang (!) and it makes it false
+
+// parseInt()
+
+//console.log(parseInt("dadadshf 12312122112 what is a number!")); //reads the first number or letter
+
+console.log("nathan".length);
+
+console.log("nathan".indexOf("a"));
+
+
+
