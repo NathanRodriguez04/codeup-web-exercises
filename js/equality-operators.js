828b42698b0c484c28abdd56823159672f8aa4b1
NathanRodriguez04 <naterod26@gmail.com>
2021-04-15 13:58:19 -0500

    apr 15

---
+++ js/equality-operators.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//loose equality
+
+//loose equality (==)
+
+var num1 = 5;
+
+var maybenum2 = "5";
+
+console.log(typeof(num1 + maybenum2));
+
+if (num1 === maybenum2){
+    console.log("these are equivalent");
+}
---
+++ js/logica-operators.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+//i want a routine which willdetermine if a dog is infact my dog
+//the dog must have fur,have whiskers, and have a name
+
+//var hasfur =true
+//var hasWhiskers = true
+//var nameIsCode = true
+
+//var isMyDog;
+
+//if(hasfur && hasWhiskers && nameIsCode){
+   // console.log("who's a good girl?? *bark bark*");
+//}
+
+
+
+//logical not(!)
+// console.log("the inverted value is; " + !true
+
+console.log("this is the weird stuff i was telling you about: " + !NaN);
