57ec894a02691f4269ebfc1096a549cf3ffb3ca8
NathanRodriguez04 <naterod26@gmail.com>
2021-04-26 15:09:26 -0500

    pushing

---
+++ js/arrays-intro-lecture.js
@@ -0,0 +1,25 @@
+(function () {
+    "use strict"
+// TODO: Make a new array which holds elements based on your own characteristics
+//  -> In a console.log, output a string which describes yourself based on those elements
+//  -> KEEP IN MIND ZERO-INDEXING!
+
+var myCharacteristics = ["nathan","web student","21"]
+    console.log(`hello i'm ${}`)
+
+
+
+// TODO: MINI-EXERCISE
+//  -> Create an array with at least 5 elements, describing whatever you'd like!
+//  -> Create a loop which will log each element to the console
+//  -> Wrap that loop in a function which has an array parameter
+//  -> Call the function and pass in your array as the argument
+//  -> DID YOU KNOW: You can add different data types to an array??
+//      -> Try adding strings, numbers, or booleans as elements
+
+var gaming =
+
+
+
+
+}())
