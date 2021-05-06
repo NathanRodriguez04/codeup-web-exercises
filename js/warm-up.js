764776cda8500094987662a654568f363754df80
NathanRodriguez04 <naterod26@gmail.com>
2021-05-05 23:32:09 -0500

    todays work

--- js/warm-up.js
+++ js/warm-up.js
@@ -2,8 +2,16 @@
 
 //Make a function named trim(string) that removes empty spaces before and after the input,
 // the returns that trimmed string
-function trim(input){
-return input.replaceAll(' ','')
+// function trim(input){
+// return input.replaceAll(' ','')
+// }
+//
+// console.log(trim("the dog got up "))
+
+
+// Write a function called last(input) that returns the last character of a string
+function last(input){
+    return input.charAt(input.length-1)
 }
 
-console.log(trim("the dog got up "))
+console.log(last("nathan"))
