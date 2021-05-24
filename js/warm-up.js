24786264d1b52bbdb9f3999bd9aedac1b69307b4
NathanRodriguez04 <naterod26@gmail.com>
2021-05-24 18:41:57 -0500

    warm up

--- js/warm-up.js
+++ js/warm-up.js
@@ -10,38 +10,39 @@
 
 
 // Write a function called last(input) that returns the last character of a string
-function last(input){
-    return input.charAt(input.length-1)
-}
-
-console.log(last("nathan"))
-
-function remainder(number, divisor){
-    return number % divisor;
-}
-
-console.log(remainder(8,2))
-console.log("-----------")
-
-function constructNum(num) {
-    let total = 0;
-    for (let i = num - 1; i >= 0; i--) {
-        total = total + num * Math.pow(10, i);
-    }
-    return total;
-}
-
-function printNumber() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(constructNum(i));
-    }
-}
-
-printNumber();
-console.log("");
-
-
-
+// function last(input){
+//     return input.charAt(input.length-1)
+// }
+//
+// console.log(last("nathan"))
+//
+// function remainder(number, divisor){
+//     return number % divisor;
+// }
+//
+// console.log(remainder(8,2))
+// console.log("-----------")
+//
+// function constructNum(num) {
+//     let total = 0;
+//     for (let i = num - 1; i >= 0; i--) {
+//         total = total + num * Math.pow(10, i);
+//     }
+//     return total;
+// }
+//
+// function printNumber() {
+//     for (let i = 1; i <= 5; i++) {
+//         console.log(constructNum(i));
+//     }
+// }
+//
+// printNumber();
+// console.log("");
+//
+//
+//
  function secondToLast(arr){
-
+     secondToLast()
  }
+
