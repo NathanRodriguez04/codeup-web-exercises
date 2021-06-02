e70be70d9443f13186b607ba054ee6cb30583693
NathanRodriguez04 <naterod26@gmail.com>
2021-06-02 10:17:50 -0500

    query

--- js/query.js
+++ js/query.js
@@ -1,7 +1,50 @@
+"use strict";
+
 // $(document).ready(function () {
-//     alert(`This webpage has loaded`);
+//     alert($("#growOnTrees").text());
 // });
 
-$("#all-caps").hide();
-$("span").hide();
-$(".show-me").show();
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// $(".codeup").css('background-color', '#FFC0CB',"1px");
+
+// $("li").css('font-size', "20px");
+
+// $(document).ready(function(){
+//     alert($("h1").text())
+// });
+
+// $(document).ready(function(){
+//     alert($("h1").html())
+// });
+
+// $("h1"[,"p"[,"li"]])
+
+// $(document).ready(function (){
+// $("h1","p","li").css('background-color', '#FFC0CB',"1px");
+// });
+
+$("h1").click(function (){
+    alert("STOP CLICKING ME!!!!");
+    $(this).css('background-color', '#FF0');
+});
+
+$("p").dblclick(function(){
+    alert("NOW LOOK AT WHAT YOU'VE DONE");
+    $(this).css("font-size","18px");
+});
+
+$("li").hover(
+    function (){
+        // alert("AHHHH!!!!!!");
+        $(this).css('color', 'red');
+    },
+    function(){
+        $(this).css('color', 'white');
+    });
+
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+});
