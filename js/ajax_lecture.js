2ef96ee589481edce2f2b1c9cb2862331cac120b
NathanRodriguez04 <naterod26@gmail.com>
2021-06-09 11:13:39 -0500

    ajax

--- js/ajax_lecture.js
+++ js/ajax_lecture.js
@@ -10,23 +10,23 @@
 //     "yearsActive": 16,
 //     "isSkilled": true,
 //     "yearRetired": null,
-//     "favoriteGames": ["valorant", "league of legends", "apex"],
+//     "favoriteGames": ["valorant", "league of legends", "cod"],
 //     "favoritePlatform": {
 //         name: "steam",
 //         bestPlatform: true,
 //     }
 // }
-// AJAX
+//AJAX
 // JQUERY REQUEST
 // $.ajax()
-//
+
 // status property tells you if the request was successful its a number,
 // 200 is minimum for successful request
 // 300 is redirected successful
 // 400 is failed request ( err 404 )
 // 500 is server error
-//
-// GET request - getting information
+
+//GET request - getting information
 // console.log($.ajax("https://jsonplaceholder.typicode.com/comments"));
 // $.ajax("https://jsonplaceholder.typicode.com/comments", {
 //     //ready to send data POST;
@@ -50,25 +50,83 @@
 //PUT - specify information in request to be EDITED
 
 
+// Responses
+
+// Failed .fail()
+// intentionally broken url to trigger 404;
+// $.ajax("https://jsonplaceholder.typie.com/posts", {
+//     type: "POST",
+//     data: {
+//         title: "test",
+//         body: "test2"
+//     }
+// }).fail(function(jqHxr, status, error){
+//     console.log(jqHxr); // returns object
+//     console.log(status); //returns statusText
+//     console.log(error);  // returns specific error
+//
+// });
+
+// both
+// $.ajax("", {
+//     type: "GET",
+//     success: function(){
+//         alert ("success");
+//     },
+//     error: function(){
+//         alert("error");
+//     }
+// });
+
+//successful
+
+// $.ajax("https://jsonplaceholder.typicode.com/posts").done(function(data, status){
+//   console.log(status);
+//     console.log(data);
+//     console.log(data[0]);
+//   // alert('posted brah');
+// })
+
 // TODO: using https://jsonplaceholder.typicode.com/ make an ajax request to the comments endpoint and console it.
-console.log($.ajax({
-    url: "https://jsonplaceholder.typicode.com/posts",
-    type: "GET",
-    data: {
-        "userId": 10,
-        "id": 10,
-        "title": "what ",
-        "body": "i dont know what im doing here"
-    }
-}));
-
-
-
 // TODO: make a POST request to the '/posts' endpoint, make sure to include any data required for that post to be made successfully
-console.log($.ajax("https://jsonplaceholder.typicode.com/cmments"));
 
-})()
+// console.log($.ajax("https://jsonplaceholder.typicode.com/comments"));
+// console.log($.ajax("https://jsonplaceholder.typicode.com/posts", {
+//     type: "POST",
+//     data: {
+//         "userId": 11,
+//         "id": 101,
+//         "title": "GG",
+//         "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
+//     }
+// }));
+
+// console.log($.ajax("https://jsonplaceholder.typicode.com/posts"));
+
+//click me form
+// $('#clickMe').click(function () {
+//     var userInput = $('#userName').val();
+//     var passInput = $('#password').val();
+//     console.log($.ajax("https://jsonplaceholder.typicode.com/posts", {
+//         type: "POST",
+//         data: {
+//             username: userInput,
+//             password: passInput,
+//         }
+//     }));
+//
+// });
 
 
+// array data
 
-// TODO: what status do you get back when making a GET request to: https://jsonplaceholder.typicode.com/cmments
+console.log($.ajax("https://jsonplaceholder.typicode.com/comments", {
+
+}).done(function(data, status){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].postId === 1){
+            console.log(data[i]);
+        }
+    }
+    console.log(status);
+}));
